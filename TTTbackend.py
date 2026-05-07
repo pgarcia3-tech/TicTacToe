48f328e510aa1f3b6c2331bea5ff0500515ba813
pgarcia3-tech <pgarcia3@bu.edu>
2026-05-06 20:39:00 -0400

    Add files via upload

--- TTTbackend.py
+++ TTTbackend.py
@@ -1,10 +1,14 @@
 
 ## BASIC BACKEND!
-# IDEAS:
-    # computer "thinks" before moving 
-    # 
+
 
 import random
+import pygame
+
+computer_pending = False
+computer_move_time = 0
+reset_pending = False
+reset_time = 0
 
 board = [
     ["", "", ""],
@@ -28,7 +32,8 @@ ties = 0
 #function to reset the game 
 
 def reset_game():
-    global board, game_over, winner
+    global board, game_over, winner, computer_pending, computer_move_time
+    global reset_pending, reset_time
 
     board = [
         ["", "", ""],
@@ -38,6 +43,10 @@ def reset_game():
     
     game_over = False
     winner = None
+    computer_pending = False
+    computer_move_time = 0
+    reset_pending = False 
+    reset_time = 0 
 
 #for every win, loss, tie, update score
 def update_score(result):
@@ -72,7 +81,34 @@ def check_winner():
     return "Tie" #check for tie
 
 #computer makes first move
+#computer tries to win by blocking player 
 def computer_move():
+    #computer tries to win, it will try to complete a row 
+    for row in range(3): 
+        for col in range(3):
+            if board[row][col] == "":
+                board[row][col] = computer
+                if check_winner() == computer:
+                    print ("Computer moved (win):", row, col)
+                    return 
+                board[row][col] = ""
+    #computer will try to block the player
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == "":
+                board [row][col] = player
+                if check_winner() == player:
+                    board[row][col] = computer
+                    print("Computer moved (block):", row, col)
+                    return 
+                board[row][col] = ""
+    #computer will try to take center 
+    if board[1][1] == "":
+        board[1][1] = computer
+        print("Computer moved (center)")
+        return
+    
+    #otherwise random
     empty_spaces = []
     
     for row in range(3):
@@ -89,11 +125,12 @@ def computer_move():
 
 #convert mouse click
 def handle_click(pos):
+    global computer_pending, computer_move_time, reset_pending, reset_time
     global winner, game_over
     x, y = pos
 
     board_x = 25 # left edge of the board
-    board_y = 75 # top of the board
+    board_y = 105 # top of the board
 
     #is the click inside the board?
     if board_x <= x <= board_x + 450 and board_y <= y <= board_y + 450:
@@ -105,7 +142,7 @@ def handle_click(pos):
 
         #if empty, place an X
 
-        if board[row][col] == "" and not game_over:
+        if board[row][col] == "" and not game_over and not computer_pending:
             board[row][col] = player
 
             result = check_winner()
@@ -113,22 +150,50 @@ def handle_click(pos):
                 winner = result
                 game_over = True
                 update_score(result)
+
+                reset_pending = True
+                reset_time = pygame.time.get_ticks() +2500
                 print("Game Over: ", winner)
                 print("Player Score: ", player_score)
                 print ("Computer Score: ", computer_score)
                 print ("Ties Score: ", ties)
                 return 
 
-            computer_move()
+            computer_pending = True
+            computer_move_time = pygame.time.get_ticks() + 400
+            return
+            
+#computer "thinks" then does something
+def update_computer():
+    global computer_pending, computer_move_time, winner, game_over
+    global reset_pending, reset_time
+    if computer_pending:
+        current_time = pygame.time.get_ticks()
 
+        if current_time >= computer_move_time:
+            computer_move()
+            computer_pending = False
             result = check_winner()
             if result is not None:
                 winner = result
                 game_over = True
                 update_score(result)
-                print("Game Over: ", winner)
-                print("Player Score: ", player_score)
-                print ("Computer Score: ", computer_score)
-                print ("Ties Score: ", ties)
-                return
+                reset_pending = True
+                reset_time = pygame.time.get_ticks() + 2500
+                print("Game Over:", winner)
+#computer updates after every game that ends
+def update_reset():
+    global reset_pending, reset_time
+    if reset_pending:
+        current_time = pygame.time.get_ticks()
+        if current_time >= reset_time:
+            reset_game()
+            reset_pending = False
+#reset button is a hard reset
+def reset_all():
+    global player_score, computer_score, ties
+    reset_game()
 
+    player_score = 0
+    computer_score = 0
+    ties = 0
--- main.py
+++ main.py
@@ -1,49 +1,224 @@
 #BASIC PYGAME WINDOW, GRID
 import pygame
 import sys
+import random
 
+import TTTbackend
+
+#initialize pygame
 pygame.init()
 
+#window dimensions
 WIDTH = 500
-HEIGHT = 600
+HEIGHT = 650
 
+#display window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic Tac Toe")
 
+# cell size in grid
 CELL_SIZE = 150
 board_x = 25
-board_y = 75
+board_y = 105
+
+#fonts
+title_font = pygame.font.SysFont(None, 52)
+font = pygame.font.SysFont(None, 100)
+med_font = pygame.font.SysFont(None, 28)
+small_font = pygame.font.SysFont(None, 26)
+
+#import ladybug and beetle drawings
+ladybug_img = pygame.image.load("ladybug.png")
+beetle_img = pygame.image.load("beetle.png")
+mantis_img = pygame.image.load("mantis2.png")
+cloud_img = pygame.image.load("cloud.png")
+
+#resize
+ladybug_img = pygame.transform.scale(ladybug_img, (100, 100))
+beetle_img = pygame.transform.scale(beetle_img, (100 ,100 ))
+mantis_img = pygame.transform.scale(mantis_img, (80, 80))
+cloud_img = pygame.transform.scale(cloud_img, (70, 70))
+
+#ladybug celebration
+celebration_ladybugs = []
+for i in range(40):
+    celebration_ladybugs.append([
+        random.randint(0, WIDTH - 50),
+        random.randint(-300, 0),
+        random.randint(6, 12)
+    ])
+
+#thunderstorm
+storm_active = False
+storm_end_time = 0
+
+#scarab swarm 
+scarab_swarm_x = -400
+
 
 def draw_grid():
-    # background
-    screen.fill((245, 245, 245))
+    # background 
+    screen.fill((117, 170, 255))
 
     # vertical lines
     for i in range(1, 3):
         pygame.draw.line(
             screen,
-            (0, 0, 0),
-            (board_x + i * CELL_SIZE, board_y),
-            (board_x + i * CELL_SIZE, board_y + 450),
-            5
+            (30,90,30), # black color
+            (board_x + i * CELL_SIZE, board_y), # start 
+            (board_x + i * CELL_SIZE, board_y + 450), # end 
+            5 # thickness
         )
 
     # horizontal lines
     for i in range(1, 3):
         pygame.draw.line(
             screen,
-            (0, 0, 0),
+            (30,90,30),
             (board_x, board_y + i * CELL_SIZE),
             (board_x + 450, board_y + i * CELL_SIZE),
             5
         )
 
-while True:
-    draw_grid()
+def draw_marks(board):
+        #draw X and O on the grid 
 
+        for row in range(3):
+            for col in range(3):
+                mark = board[row][col]
+                if mark != "": #if mark isnt empty
+
+                    # text = font.render(mark, True, color)
+                    x = board_x + col * CELL_SIZE + CELL_SIZE // 2 - 50
+                    y = board_y + row * CELL_SIZE + CELL_SIZE // 2 - 50
+
+                    if mark == "X":
+                        screen.blit(ladybug_img, (x, y))
+                    else:
+                        screen.blit(beetle_img, (x, y))
+
+def draw_restart_button():
+    button_color = (200, 240, 200)
+    pygame.draw.rect(screen, button_color, (160, 600, 180, 35), border_radius=30)
+    pygame.draw.rect(screen, (0,0,0), (160, 600, 180, 35), 2, border_radius=30)
+
+    text = small_font.render("THUNDERSTORM", True, (0,0,0))
+    screen.blit(
+        text,
+        (
+            160 + 180 // 2- text.get_width() // 2,
+            600 + 35 // 2- text.get_height() // 2
+        )
+    )
+
+def draw_title():
+    title = title_font.render("Tic Tac Toe", True, (30, 30, 30))
+
+    screen.blit(title, (WIDTH // 2 - title.get_width() // 2, 10))
+    #mantis image - he wants YOU to protect his garden
+    screen.blit(mantis_img, (15, 8))
+ 
+#status at the bottom!
+def draw_status():
+    if TTTbackend.winner == "X":
+        message = "Ladybugs win :D "
+    elif TTTbackend.winner == "O":
+        message = "Scarabs feast tonight >:D "
+    elif TTTbackend.winner == "Tie":
+        message = "The garden rests..."
+    else:
+        message = "Defend Old Mr. Mantis' Garden"
+    status_text = med_font.render(message, True, (40, 70, 40))
+    screen.blit(status_text, (WIDTH // 2 - status_text.get_width() // 2, 55))
+
+    score_text = small_font.render(
+        f"Ladybugs: {TTTbackend.player_score}   Scarabs: {TTTbackend.computer_score}   Ties {TTTbackend.ties}", True, (0, 0, 0) )
+    screen.blit(score_text, (WIDTH // 2 - score_text.get_width() // 2, 575))
+#cloud for decoration
+def draw_cloud():
+    screen.blit(cloud_img, (390, 5))
+    
+# ladybugs rain every time they win
+def draw_ladybug_celebration():
+    if TTTbackend.winner == "X":
+        for bug in celebration_ladybugs:
+            x, y, speed = bug
+
+            screen.blit(
+                pygame.transform.scale(ladybug_img, (45, 45)),
+                (x, y)
+            )
+
+            bug[1] += speed
+
+            if bug[1] > HEIGHT:
+                bug[0] = random.randint(0, WIDTH - 45)
+                bug[1] = random.randint(-200, -45)
+                bug[2] = random.randint(6, 12)
+
+# flashing red every time the scarabs win
+def draw_scarab_swarm():
+    global scarab_swarm_x
+    if TTTbackend.winner == "O":
+        for i in range(30):
+            x = scarab_swarm_x + random.randint(0, 900)
+            y = random.randint(90, 500)
+
+            screen.blit(
+                pygame.transform.scale(beetle_img, (60, 60)),
+                (x, y)
+            )
+
+        scarab_swarm_x += 10
+        if scarab_swarm_x > WIDTH:
+            scarab_swarm_x = -500
+    else:
+        scarab_swarm_x = -500
+
+#thunderstorm hard reset
+def draw_thunderstorm():
+    if storm_active:
+        current_time = pygame.time.get_ticks()
+        # flashing effect
+        if (current_time // 80) % 2 == 0:
+            screen.fill((255, 255, 120)) #yellow flash
+        else:
+            screen.fill((240, 240, 240))  # white flash
+
+
+            
+# game loop, runs continuously
+while True:
+    TTTbackend.update_computer()
+    TTTbackend.update_reset()
+    draw_grid()
+    
+    draw_marks(TTTbackend.board)
+    draw_title()
+    draw_status()
+    draw_restart_button()
+    draw_cloud()
+    draw_ladybug_celebration()
+    draw_scarab_swarm()
+    draw_thunderstorm()
+    
+# check for event, closing window, etc
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: 
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            if 160 <= x <= 340 and 600 <= y <= 635:
+                storm_active = True
+                storm_end_time = pygame.time.get_ticks() + 800
+                TTTbackend.reset_all()
+            else:
+                TTTbackend.handle_click(event.pos)
+    current_time = pygame.time.get_ticks()
+    if storm_active and current_time >= storm_end_time:
+        storm_active = False
 
+
+# update display
     pygame.display.update()
